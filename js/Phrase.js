79300ce37eac9d1f670cd78a58a363c161ae9090
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-11-15 16:12:16 +0100

    additional commenting of Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,8 +1,15 @@
 class Phrase {
+  /**
+   * 
+   * @param {string} phrase - a string pulled from an array of strings in data.js
+   */
   constructor(phrase) {
     this.phrase = phrase;
   }
 
+  /**
+   * Function athat adds the blank boxes representing letters to the display
+   */
   addPhraseToDisplay() {
     const phraseUl = $("#phrase ul");
     let html = "";
@@ -14,10 +21,19 @@ class Phrase {
     phraseUl.html(html);
   }
 
+  /**
+   * 
+   * @param {string} letter - string that contains the letter to search for
+   * @returns {boolean} - returns true or false depending on whether or not the letter was found in the phrase  
+   */
   checkLetter(letter) {
     return this.phrase.includes(letter);
   }
 
+  /**
+   * Function that loops through all the boxes and searches for the matching letter and reveals all instances of it
+   * @param {string} letter - the letter we're going to reveal 
+   */
   showMatchedLetter(letter) {
     let boxes = $("#phrase li");
     for(let box of boxes) {
